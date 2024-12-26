0d39143bc470b72842ec8fdf05ceae85dbff5ac6
Amane <ayane901116@email.com>
2024-12-26 23:24:04 +0900

    complete

---
+++ JS/aos.js
@@ -0,0 +1,20 @@
+//----------------scroll trigger-----------------------------------
+$(document).ready(function( ){ 
+    gsap.registerPlugin(ScrollTrigger); 
+})
+
+function isElementInViewport(el) {
+    const rect = el.getBoundingClientRect();
+    return rect.bottom < 0 || rect.top > window.innerHeight;
+}
+
+function addClassToVisibleElements() {
+    var aosElements = document.querySelectorAll(".aos");
+    aosElements.forEach(function (aosElement) {
+        if (!isElementInViewport(aosElement)) aosElement.classList.add("ed");
+        else aosElement.classList.remove("ed");
+    });
+}
+
+document.addEventListener("scroll", addClassToVisibleElements);
+addClassToVisibleElements();
